27db0f1a20c95ebf4da3d91b7cd7944342dd3a55
fordreram2018 <fordream2018@126.com>
2018-12-10 04:29:20 +0800

    first

---
+++ src/main/java/com/fordream/boot/controller/MainHomeController.java
@@ -0,0 +1,15 @@
+package com.fordream.boot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainHomeController {
+	@ResponseBody
+	@RequestMapping("/hello")
+	public String hello() {
+		return "Hello World";
+	}
+
+}
